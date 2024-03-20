4be53db11f01b403008c807c6551227411607367
tywiththewifi <156716969+tywiththewifi@users.noreply.github.com>
2024-03-20 14:23:28 -0700

    Finish javascript logic and begin woeking on css structure

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const authorInput = document.getElementById("author");
 const pagesInput = document.getElementById("pages");
 const readBookInput = document.getElementById("read");
 
+const container = document.querySelector(".container");
+const bookCards = document.querySelector(".bookCards");
 
 const myLibrary = [];
 
@@ -23,7 +25,13 @@ cancelButton.addEventListener("click", () => {
 
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
+    let newBook = new Book(titleInput.value, 
+        authorInput.value, 
+        parseInt(pagesInput.value), 
+        readBookInput.checked);
     
+    addBooktoLibrary(newBook);
+    displayBook(newBook);
     newBookPopUp.close();
     clearForm();
 })
@@ -35,20 +43,12 @@ function clearForm() {
     readBookInput.checked = true;
 }
 
-
-
 class Book {
     constructor (title, author, pages, haveRead) {
         this.title = title;
         this.author = author;
-        this.pages = number;
-        this.haveRead = function() {
-            if (true) {
-                return "have read";
-            } else {
-                return "not read yet";
-            }
-        };
+        this.pages = pages;
+        this.haveRead = haveRead;
         this.info = function() {
             console.log(this.title + " by " + this.author + ", " + this.pages + " pages, " + this.haveRead)
         };
@@ -59,7 +59,12 @@ class Book {
         return myLibrary.length;
     };
 
+    toggleStatus() {
+        this.read = this.read ? false : true;
 };
+
+};
+
 
 function addBooktoLibrary(newBook) {
     newBook.assignID();
@@ -68,4 +73,50 @@ function addBooktoLibrary(newBook) {
     return `${newBook.title} added to the library! ID: ${newBook.bookID}`;
 }
 
+function displayBook(book) {
+    let card = document.createElement('div');
+    card.classList = 'card';
+    card.setAttribute('data-book-id', book.bookID);
+    let title = document.createElement('div');
+    title.textContent = `${book.title}`;
+    let author = document.createElement('div');
+    author.textContent = `${book.author}`;
+    let pages = document.createElement('div');
+    pages.textContent = `${book.pages} ${book.pages === 1 ? " page" : " pages"}`;
+    let read = document.createElement('button');
+    read.textContent = `${book.read ? "Read" : "Not Read"}`
+    read.classList = `${book.read ? "read" : "not read"} grow`;
+    read.style.width = '80px';
+    read.style.height = '30px';
+    read.addEventListener('click', () => {
+        book.toggleStatus();
+        read.textContent = `${book.read ? "Read" : "Not Read"}`;
+        read.classList = `${book.read ? "read" : "not read"}`;
+    });
+
+    let deleteButton = document.createElement('img');
+    deleteButton.src = "images/trash-can-outline.svg";
+    deleteButton.style.height = '20px';
+    deleteButton.classList = 'remove grow';
+    deleteButton.addEventListener('click', () => {
+        let deleteIndex = myLibrary.findIndex(b => b.bookID === book.bookID);
+        if (deleteIndex !== -1) {
+        myLibrary.splice(deleteIndex, 1);
+        bookCards.removeChild(card);
+        }
+    });
+    card.appendChild(deleteButton);
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    bookCards.appendChild(card);
+}
+
+function displayBooks() {
+    for (book of myLibrary);
+        displayBook(book);
+}
+
+displayBooks();
 
